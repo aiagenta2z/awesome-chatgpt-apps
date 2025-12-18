533b145aee7c6d53553f4924216cbeb0aff1feac
AI-Hub-Admin <aihubadmin@126.com>
2025-12-18 14:28:06 +0800

    update

---
+++ registry/run_register_chatgpt_apps.sh
@@ -0,0 +1,26 @@
+
+## Generation Marketplace Key at https://www.deepnlp.org/workspace/keys for scene 'ai_agent_marketplace_api',
+## Visit https://github.com/aiagenta2z/agtm for detail usages
+export AI_AGENT_MARKETPLACE_ACCESS_KEY="{YOUR_ACCESS_KEY}"
+# test
+export AI_AGENT_MARKETPLACE_ACCESS_KEY="TEST_KEY_AI_AGENT_REGISTRY"
+
+agtm upload --config ../meta/0xkoller_arcade.json
+agtm upload --config ../meta/harijoe_interactive-piano.json
+agtm upload --config ../meta/pascal-monito_monito.json
+agtm upload --config ../meta/achieving100ms_memories-video-editor.json
+agtm upload --config ../meta/dtorourke01_ultimate-broker-route.json
+agtm upload --config ../meta/jjjulllesss_14ers.json
+agtm upload --config ../meta/rodriguespn_study-buddy.json
+agtm upload --config ../meta/aiagenta2z_bill-agent.json
+agtm upload --config ../meta/dustin-wilson_autoagent.json
+agtm upload --config ../meta/lakshya-bakshi_heron-health.json
+agtm upload --config ../meta/strive_math_pdf-generator.json
+agtm upload --config ../meta/alpic-ai_murder-in-the-valley.json
+agtm upload --config ../meta/enzo-reader_enzo-reader.json
+agtm upload --config ../meta/nanjuan_sophtron.json
+agtm upload --config ../meta/udaykakade25_marketflux.json
+agtm upload --config ../meta/bmc_askmymoney.json
+agtm upload --config ../meta/frederic-barthelet_times-up.json
+agtm upload --config ../meta/nicholas-dickey_findexar.json
+agtm upload --config ../meta/zhongxinuk_texas-duel.json
